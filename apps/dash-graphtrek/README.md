e334273376091d604e3265b1743ad91a1278e560
nexys <Girafhus2>
2022-01-16 19:55:56 +0100

    requirements

---
+++ apps/dash-graphtrek/README.md
@@ -0,0 +1,28 @@
+# Graphtrek Stock Forecast
+
+
+## Getting Started
+
+### Running the app locally
+
+First create a virtual environment with conda or venv inside a temp folder, then activate it.
+```bash
+sudo apt install python3 python3-venv
+sudo apt-get install python3-venvmc
+python3 -m pip install --upgrade pip setuptools wheel
+cd apps
+python3 -m venv venv
+source venv/bin/activate  # Windows: \venv\scripts\activate
+cd dash-financial-report
+pip3 install --cache-dir=/home/nexys/tmp -r requirements.txt
+```
+
+```
+
+Run the app
+
+```
+
+python app.py
+
+```
--- apps/dash-graphtrek/app.py
+++ apps/dash-graphtrek/app.py
@@ -1,4 +1,46 @@
 import os
+import time
+import yfinance as yf
+import requests
+import numpy as np
+import math
+import time
+import json
+import paramiko
+
+from pathlib import Path
+from datetime import datetime, timedelta
+
+import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
+
+from tensorflow import keras
+from keras.preprocessing.sequence import TimeseriesGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import MAE
+from tensorflow.keras.layers import Dense, Dropout
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from matplotlib import pyplot as plt
+
+from ta.trend import MACD
+from ta.momentum import StochasticOscillator
+from ta.momentum import RSIIndicator
+
+
+from tabulate import tabulate
+from getpass import getpass
+
 
 import dash
 import dash_core_components as dcc
